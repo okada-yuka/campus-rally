0078299da00b2cb8a400bd351814c708d2d22bb9
Yuka Okada <okada2019@sil.doshisha.ac.jp>
2021-04-17 15:17:28 +0900

    :seedling: 写真を撮影する機能を追加した

--- campus-app/campus-app/MapViewController.swift
+++ campus-app/campus-app/MapViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
