dfa75cad0a0c2de250a5af895511f696adc5aeb5
Yuka Okada <okada2019@sil.doshisha.ac.jp>
2021-04-18 20:55:51 +0900

    :seedling: 香知館の画面を設定した

--- campus-app/campus-app/MapViewController.swift
+++ campus-app/campus-app/MapViewController.swift
@@ -2,66 +2,94 @@
 //  MapViewController.swift
 //  campus-app
 //
-//  Created by Yuka Okada on 2021/04/17.
+//  Created by Yuka Okada on 2021/04/18.
 //
 
 import UIKit
-import AWSMobileClient
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
     
-    let appDelegate = UIApplication.shared.delegate as! AppDelegate
-    @IBOutlet weak var usernameLabel: UILabel!
-    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+
+        if annotation is MKUserLocation {
+            //return nil so map view draws "blue dot" for standard user location
+            return nil
+        }
+
+        let reuseId = "pin"
+        let  pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+        pinView.canShowCallout = true
+        pinView.animatesDrop = true
+        let btn = UIButton(type: .detailDisclosure)
+        btn.addTarget(self, action: #selector(buttonEvent(_:)), for: UIControl.Event.touchUpInside)
+        pinView.rightCalloutAccessoryView = btn
+        return pinView
+    }
+
+    //    ピンの中のiマークを押したときの処理
+    @objc func buttonEvent(_ sender: UIButton) {
+        let storyboard: UIStoryboard = self.storyboard!
+               // ②遷移先ViewControllerのインスタンス取得
+               let nextView = storyboard.instantiateViewController(withIdentifier: "PinView")
+               // ③画面遷移
+               self.present(nextView, animated: true, completion: nil)
+
+
+    }
+
+    //    ピンを立てる
+    func addAnnotation( latitude: CLLocationDegrees,longitude: CLLocationDegrees, title:String, subtitle:String) {
+        mapView.delegate = self
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+        annotation.title = title
+        annotation.subtitle = subtitle
+        self.mapView.addAnnotation(annotation)
+    }
+
+
+
+
+    @IBOutlet weak var mapView: MKMapView!
+    //CLLocationManagerの入れ物を用意
+    var myLocationManager:CLLocationManager!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        // Do any additional setup after loading the view.
-        AWSMobileClient.sharedInstance().initialize { (UserState, error) in
-            
-            if let userState = UserState {
-                switch (UserState) {
-                case .signedIn:
-                    DispatchQueue.main.async {
-                        self.appDelegate.username = AWSMobileClient.default().username
-                        print(AWSMobileClient.default().username)
-                        print(self.appDelegate.username)
-                        print("Logged In")
-                        self.usernameLabel.text = self.appDelegate.username + "さん"
-                    }
-                    
-                case .signedOut:
-                    AWSMobileClient.sharedInstance().showSignIn(navigationController: self.navigationController!, { (UserState, error) in
-                        if (error == nil){ //サインイン成功時
-                            DispatchQueue.main.async {
-                                self.appDelegate.username = AWSMobileClient.default().username
-                                print(AWSMobileClient.default().username)
-                                print("Sign In")
-                                self.usernameLabel.text = self.appDelegate.username + "さん"
-                            }
 
-                        }
+        //CLLocationManagerをインスタンス化
+        myLocationManager = CLLocationManager()
+        //位置情報使用許可のリクエストを表示するメソッドの呼び出し
+        myLocationManager.requestWhenInUseAuthorization()
 
-                    })
-                default:
-                    AWSMobileClient.sharedInstance().signOut()
-                    self.appDelegate.username = AWSMobileClient.default().username
-                }
-                
- 
-            } else if let error = error {
-                print(error.localizedDescription)
-            }
-            
-            print("username")
-            print(AWSMobileClient.default().username)
-            print("username")
-        }
+        // 中心地はユーザー現在地
+
+        mapView.setCenter(mapView.userLocation.coordinate, animated: true)
+        mapView.userTrackingMode = MKUserTrackingMode.follow
+
+        // 縮尺を設定
+        var region = mapView.region
+        region.center = mapView.userLocation.coordinate
+        region.span.latitudeDelta = 0.01
+        region.span.longitudeDelta = 0.01
+        // マップビューに縮尺を設定
+        mapView.setRegion(region, animated:true)
+
+        // 以下ピン
+        addAnnotation(latitude: 34.80141666723223,  longitude: 135.77055102690483, title: "ラーネッド記念図書館", subtitle: "")
+        addAnnotation(latitude: 34.80080759819321,  longitude: 135.76798711156198, title: "香知館　KC", subtitle: "社会情報学研究室 - SIL")
         
         
+        self.navigationController?.navigationBar.isHidden = true
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+
     }
-    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
 
 }
-
