4f0af75837f99ba058e5a30f1abe6445d7d3a07f
Yuka Okada <okada2019@sil.doshisha.ac.jp>
2021-04-18 19:19:03 +0900

    :seedling: マップ上のピンから各施設のクエスト画面への遷移を実装した

---
+++ campus-app/campus-app/MapKitViewController.swift
@@ -0,0 +1,89 @@
+//
+//  MapKitViewController.swift
+//  campus-app
+//
+//  Created by Yuka Okada on 2021/04/18.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapKitViewController: UIViewController, MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+
+        if annotation is MKUserLocation {
+            //return nil so map view draws "blue dot" for standard user location
+            return nil
+        }
+
+        let reuseId = "pin"
+        let  pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+        pinView.canShowCallout = true
+        pinView.animatesDrop = true
+        let btn = UIButton(type: .detailDisclosure)
+        btn.addTarget(self, action: #selector(buttonEvent(_:)), for: UIControl.Event.touchUpInside)
+        pinView.rightCalloutAccessoryView = btn
+        return pinView
+    }
+
+    //    ピンの中のiマークを押したときの処理
+    @objc func buttonEvent(_ sender: UIButton) {
+        let storyboard: UIStoryboard = self.storyboard!
+               // ②遷移先ViewControllerのインスタンス取得
+               let nextView = storyboard.instantiateViewController(withIdentifier: "PinView")
+               // ③画面遷移
+               self.present(nextView, animated: true, completion: nil)
+
+
+    }
+
+    //    ピンを立てる
+    func addAnnotation( latitude: CLLocationDegrees,longitude: CLLocationDegrees, title:String, subtitle:String) {
+        mapView.delegate = self
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+        annotation.title = title
+        annotation.subtitle = subtitle
+        self.mapView.addAnnotation(annotation)
+    }
+
+
+
+    @IBOutlet weak var mapView: MKMapView!
+    //CLLocationManagerの入れ物を用意
+    var myLocationManager:CLLocationManager!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //CLLocationManagerをインスタンス化
+        myLocationManager = CLLocationManager()
+        //位置情報使用許可のリクエストを表示するメソッドの呼び出し
+        myLocationManager.requestWhenInUseAuthorization()
+
+        //        / 以下を追加 ///
+        //        中心地はユーザー現在地
+
+        mapView.setCenter(mapView.userLocation.coordinate, animated: true)
+        mapView.userTrackingMode = MKUserTrackingMode.follow
+
+        // 縮尺を設定
+        var region = mapView.region
+        region.center = mapView.userLocation.coordinate
+        region.span.latitudeDelta = 0.01
+        region.span.longitudeDelta = 0.01
+        // マップビューに縮尺を設定
+        mapView.setRegion(region, animated:true)
+
+        //        以下ピン
+        addAnnotation(latitude: 35.689830,  longitude: 139.697320, title: "場所１", subtitle: "サブタイトル")
+        addAnnotation(latitude: 35.683082,  longitude: 139.701510, title: "場所2", subtitle: "サブタイトル")
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
