64a7fa8aebcc20c3212e5c4a30f27b485ee832eb
Yuka Okada <okada2019@sil.doshisha.ac.jp>
2021-04-19 10:01:01 +0900

    :dizzy: 各施設ごとにボタン押下時の関数を設定した #1

--- campus-app/campus-app/AppDelegate.swift
+++ campus-app/campus-app/AppDelegate.swift
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var id: String!
     var username: String!
+    
+    var pin_title: String!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
