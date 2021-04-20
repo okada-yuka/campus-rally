298730f9a6ad99ba289f797f0bb149a096a8b91e
Yuka Okada <okada2019@sil.doshisha.ac.jp>
2021-04-21 02:11:49 +0900

    :seedling: Cognitoでサインイン機能を実装した #4

--- campus-rally/MapViewController.swift
+++ campus-rally/MapViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 import MapKit
 import CoreLocation
+import AWSMobileClient
 
 class MapViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var usernameLabel: UILabel!
     
     var appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
     
@@ -46,7 +48,47 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    
+        // Cognitoでのログイン
+        AWSMobileClient.sharedInstance().initialize { (UserState, error) in
+            
+            if let userState = UserState {
+                switch (UserState) {
+                case .signedIn:
+                    DispatchQueue.main.async {
+                        self.appDelegate.username = AWSMobileClient.default().username
+                        print(AWSMobileClient.default().username)
+                        print(self.appDelegate.username)
+                        print("Logged In")
+                        self.usernameLabel.text = self.appDelegate.username + "さん"
+                    }
+                    
+                case .signedOut:
+                    AWSMobileClient.sharedInstance().showSignIn(navigationController: self.navigationController!, { (UserState, error) in
+                        if (error == nil){ //サインイン成功時
+                            DispatchQueue.main.async {
+                                self.appDelegate.username = AWSMobileClient.default().username
+                                print(AWSMobileClient.default().username)
+                                print("Sign In")
+                                self.usernameLabel.text = self.appDelegate.username + "さん"
+                            }
+
+                        }
+
+                    })
+                default:
+                    AWSMobileClient.sharedInstance().signOut()
+                    self.appDelegate.username = AWSMobileClient.default().username
+                }
+                
+ 
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            print("username")
+            print(AWSMobileClient.default().username)
+            print("username")
+        }
         
         initMap()
         mapView.delegate = self
